e41dfd5bbcae09eea4d323a25d8355900eeeeeab
amirhallaji <a.hallaji.b@gmail.com>
2020-11-22 14:39:19 +0330

    HW4 - finished coding and starting to fix the errors.

--- HW4/dsd-32-bit-universal-barrel-shifter/shifter_tb.vhd
+++ HW4/dsd-32-bit-universal-barrel-shifter/shifter_tb.vhd
@@ -17,7 +17,7 @@ ARCHITECTURE test of shifter_tb IS
         dir     : IN std_logic;
         nrst    : IN std_logic;
         sout    : OUT std_logic;
-        dout     : OUT std_logic_vector(31 DOWNTO 0)
+        dout    : OUT std_logic_vector(31 DOWNTO 0)
     );
     END COMPONENT;
 
@@ -39,5 +39,8 @@ ARCHITECTURE test of shifter_tb IS
         --Input assignments--
         ---------------------
         clk_t <= NOT clk_t AFTER 5 ns;
-        
+        din_t = X'0007';
+        sham_t = X'0000' AFTER 10 ns  ;
+        shty_t = B'00', B'01' AFTER 20 ns, B'10' AFTER 30 ns, B'11 AFTER 40 ns;
+        dir_t = '0', '1' AFTER 50 ns; 
 END test;

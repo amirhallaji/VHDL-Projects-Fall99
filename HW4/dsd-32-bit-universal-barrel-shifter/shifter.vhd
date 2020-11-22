f7673152d1f77642c1b7c6ec4fdf15c7bc34baa8
amirhallaji <a.hallaji.b@gmail.com>
2020-11-22 18:12:05 +0330

    shifter.vhd and shifter_tb.vhd is completed but has error loading design.

--- HW4/dsd-32-bit-universal-barrel-shifter/shifter.vhd
+++ HW4/dsd-32-bit-universal-barrel-shifter/shifter.vhd
@@ -1,30 +1,35 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY shifter IS
+ENTITY alu IS
     PORT (
+        clk     : IN std_logic;
+        nrst    : IN std_logic;
         din     : IN std_logic_vector(31 DOWNTO 0);
+        sin     : IN std_logic;
         sham    : IN std_logic_vector(4 DOWNTO 0);
         shty    : IN std_logic_vector(1 DOWNTO 0);
-        sin     : IN std_logic;
-        clk     : IN std_logic;
         dir     : IN std_logic;
-        nrst    : IN std_logic;
         sout    : OUT std_logic;
-        dout     : OUT std_logic_vector(31 DOWNTO 0)
+        dout    : OUT std_logic_vector(31 DOWNTO 0)
     );
-END shifter;
+END alu;
 
-ARCHITECTURE behavioral OF shifter IS
+ARCHITECTURE behavioral OF alu IS
 
     SIGNAL temp         : std_logic_vector(31 DOWNTO 0);
     SIGNAL sout_sig     : std_logic;
+    SIGNAL sham_int     : integer;
+    SIGNAL zero         : STD_LOGIC_VECTOR (31 DOWNTO 0) :=  "00000000000000000000000000000000";
+	SIGNAL one          : STD_LOGIC_VECTOR (31 DOWNTO 0) :=  "11111111111111111111111111111111";
 
 BEGIN
-    PROCESS (clk)
 
-        VARIABLE sham_int : to_integer(unsigned(sham));
+    sham_int <= to_integer(unsigned(sham));
+
+    PROCESS (clk)
 
     BEGIN
         IF clk = '1' THEN
@@ -34,19 +39,45 @@ BEGIN
             ELSE
                 IF shty = "00" THEN     -- logical
                     IF dir = '0' THEN
+                        sout_sig <= temp(32-sham_int);
+                        IF sin = '0' THEN
+                            temp <= din(31 - sham_int DOWNTO 0) & zero(sham_int-1 DOWNTO 0);
+                        ELSE
+                            temp <= din(31 - sham_int DOWNTO 0) & one(sham_int-1 DOWNTO 0);
+                        END IF;
                     ELSE
+                        sout_sig <= temp(sham_int-1);
+                        IF sin = '0' THEN
+                            temp <= zero(sham_int-1 DOWNTO 0) & din(31 DOWNTO sham_int);
+                        ELSE
+                            temp <= one(sham_int-1 DOWNTO 0) & din(31 DOWNTO sham_int);
+                        END IF;
                     END IF;
 
                 ELSIF shty = "01" THEN  -- arithmetic
                     IF dir = '0' THEN
+                        sout_sig <= temp(32-sham_int);
+                        IF sin = '0' THEN
+                            temp <= din(31 - sham_int DOWNTO 0) & zero(sham_int-1 DOWNTO 0);
+                        ELSE
+                            temp <= din(31 - sham_int DOWNTO 0) & one(sham_int-1 DOWNTO 0);
+                        END IF;
                     ELSE
+                        sout_sig <= temp(sham_int-1);
+                        IF din(31) = '0' THEN
+                            temp <= zero(sham_int-1 DOWNTO 0) & din(31 DOWNTO sham_int);
+                        ELSE
+                            temp <= one(sham_int-1 DOWNTO 0) & din(31 DOWNTO sham_int);
+                        END IF;
                     END IF;
 
                 ELSIF shty = "10" THEN  -- circular
                     IF dir = '0' THEN
-                        temp <= din(sham_int DOWNTO 0) & din(31 DOWNTO (sham_int+1));
+                        sout_sig <= temp(32-sham_int);
+                        temp <= din(31 - sham_int DOWNTO 0) & din(sham_int-1 DOWNTO 0);
                     ELSE
-                        temp <= din((sham_int-1) DOWNTO 0) & din(31 DOWNTO sham_int);
+                        sout_sig <= temp(sham_int-1);
+                        temp <= din(sham_int-1 DOWNTO 0) & din(31 DOWNTO sham_int);
                     END IF;
 
                 ELSE                    -- register
--- HW4/dsd-32-bit-universal-barrel-shifter/shifter_tb.vhd
+++ HW4/dsd-32-bit-universal-barrel-shifter/shifter_tb.vhd
@@ -9,13 +9,13 @@ ARCHITECTURE test of shifter_tb IS
     COMPONENT shifter IS
 
     PORT(
+        clk     : IN std_logic;
+        nrst    : IN std_logic;
         din     : IN std_logic_vector(31 DOWNTO 0);
+        sin     : IN std_logic;
         sham    : IN std_logic_vector(4 DOWNTO 0);
         shty    : IN std_logic_vector(1 DOWNTO 0);
-        sin     : IN std_logic;
-        clk     : IN std_logic;
         dir     : IN std_logic;
-        nrst    : IN std_logic;
         sout    : OUT std_logic;
         dout    : OUT std_logic_vector(31 DOWNTO 0)
     );
@@ -33,14 +33,14 @@ ARCHITECTURE test of shifter_tb IS
         --- INSTANTIATION----
         ---------------------
 
-        SH_INS : shifter PORT MAP(din_t, sham_t, shty_t, sin_t, clk_t, dir_t, nrst_t, sout_t, dout_t);
+        SH_INS : shifter PORT MAP(clk_t, nrst_t, din_t, sin_t, sham_t, shty_t, dir_t, sout_t, dout_t);
 
         ---------------------
         --Input assignments--
         ---------------------
         clk_t <= NOT clk_t AFTER 5 ns;
-        din_t <= X"07";
-        sham_t <= X"00" AFTER 10 ns  ;
-        shty_t <= B"00", B"01" AFTER 20 ns, B"10" AFTER 30 ns, B"11" AFTER 40 ns;
-        dir_t <= '0', '1' AFTER 50 ns; 
+        din_t <=  X"00000007";
+        sham_t <= "00000" AFTER 12 ns  ;
+        shty_t <= "00", "01" AFTER 24 ns, "10" AFTER 36 ns, "11" AFTER 42 ns;
+        dir_t <= '0', '1' AFTER 58 ns; 
 END test;
